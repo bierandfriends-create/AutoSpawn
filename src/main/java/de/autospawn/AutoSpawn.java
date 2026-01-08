db3f386df928df78cf85f45039a33aca071a9cd6
bierandfriends-create <bierandfriends@gmail.com>
2026-01-08 20:06:54 +0100

    Add world check after delay

--- src/main/java/de/autospawn/AutoSpawn.java
+++ src/main/java/de/autospawn/AutoSpawn.java
@@ -1,17 +1,36 @@
 package de.autospawn;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public final class AutoSpawn extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage("§a[AutoSpawn] Plugin wurde erfolgreich geladen!");
+        Bukkit.getConsoleSender().sendMessage("§a[AutoSpawn] Plugin gestartet, warte 15 Sekunden...");
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                checkWorld("farmwelt");
+                checkWorld("Nether");
+            }
+        }.runTaskLater(this, 20L * 15); // 15 Sekunden
+    }
+
+    private void checkWorld(String worldName) {
+        World world = Bukkit.getWorld(worldName);
+        if (world == null) {
+            Bukkit.getConsoleSender().sendMessage("§c[AutoSpawn] Welt NICHT geladen: " + worldName);
+        } else {
+            Bukkit.getConsoleSender().sendMessage("§a[AutoSpawn] Welt geladen: " + worldName);
+        }
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage("§c[AutoSpawn] Plugin wurde deaktiviert!");
+        Bukkit.getConsoleSender().sendMessage("§c[AutoSpawn] Plugin deaktiviert.");
     }
 }
